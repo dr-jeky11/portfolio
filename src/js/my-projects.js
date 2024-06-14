30444f427faa29280fd83fcc7fa482ca182ea8a5
Darina Ivashchenko <114852364+KittsWolves0@users.noreply.github.com>
2024-06-14 19:08:22 +0300

    fix img problem

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -1,64 +1,64 @@
-const imgArr = [{
-    url: "/img/my-projects/proj-1-x1.jpg",
-    urlX2: "/img/my-projects/proj-1-x2.jpg",
-    title: "wallet webservice"
-},
+import imgFourS from '../img/my-projects/proj-4-x1.jpg';
+import imgFourL from '../img/my-projects/proj-4-x2.jpg';
+import imgFiveS from '../img/my-projects/proj-5-x1.jpg';
+import imgFiveL from '../img/my-projects/proj-5-x2.jpg';
+import imgSixS from '../img/my-projects/proj-6-x1.jpg';
+import imgSixL from '../img/my-projects/proj-6-x2.jpg';
+import imgSevenS from '../img/my-projects/proj-7-x1.jpg';
+import imgSevenL from '../img/my-projects/proj-7-x2.jpg';
+import imgEigthS from '../img/my-projects/proj-8-x1.jpg';
+import imgEigthL from '../img/my-projects/proj-8-x2.jpg';
+import imgNineS from '../img/my-projects/proj-9-x1.jpg';
+import imgNineL from '../img/my-projects/proj-9-x2.jpg';
+import imgTenS from '../img/my-projects/proj-10-x1.jpg';
+import imgTenL from '../img/my-projects/proj-10-x2.jpg';
+
+const imgArr = [
 {
-    url: "/img/my-projects/proj-2-x1.jpg",
-    urlX2: "/img/my-projects/proj-2-x2.jpg",
-    title: "Green harvest webservice"
-},
-{
-    url: "/img/my-projects/proj-3-x1.jpg",
-    urlX2: "/img/my-projects/proj-3-x2.jpg",
-    title: "English Exellence webservice"
-},
-{
-    url: "/img/my-projects/proj-4-x1.jpg",
-    urlX2: "/img/my-projects/proj-4-x2.jpg",
+    url: imgFourS,
+    urlX2: imgFourL,
     title: "power pulse webservice"
 },
 {
-    url: "/img/my-projects/proj-5-x1.jpg",
-    urlX2: "/img/my-projects/proj-5-x2.jpg",
+    url: imgFiveS,
+    urlX2: imgFiveL,
     title: "mimino website"
 },
 {
-    url: "/img/my-projects/proj-6-x1.jpg",
-    urlX2: "/img/my-projects/proj-6-x2.jpg",
+    url: imgSixS,
+    urlX2: imgSixL,
     title: "vyshyvanka vibes Landing Page"
 },
 {
-    url: "/img/my-projects/proj-7-x1.jpg",
-    urlX2: "/img/my-projects/proj-7-x2.jpg",
+    url: imgSevenS,
+    urlX2: imgSevenL,
     title: "chego jewelry website"
 },
 {
-    url: "/img/my-projects/proj-8-x1.jpg",
-    urlX2: "/img/my-projects/proj-8-x2.jpg",
+    url: imgEigthS,
+    urlX2: imgEigthL,
     title: "energy flow webservice"
 },
 {
-    url: "/img/my-projects/proj-9-x1.jpg",
-    urlX2: "/img/my-projects/proj-9-x2.jpg",
+    url: imgNineS,
+    urlX2: imgNineL,
     title: "fruitbox online store"
 },
 {
-    url: "/img/my-projects/proj-10-x1.jpg",
-    urlX2: "/img/my-projects/proj-10-x2.jpg",
+    url: imgTenS,
+    urlX2: imgTenL,
     title: "starlight studio landing page"
 },
 ];
 
 const list = document.querySelector(".projects-list");
 const loadBtn = document.querySelector(".projects-button");
-let loadNumber = 3;
+let loadNumber = 0;
 
 loadBtn.addEventListener("click", hendleLoad);
 
 
 function hendleLoad () {
-    console.log(loadNumber);
 
 list.insertAdjacentHTML("beforeend", renderProjectList(imgArr));
 if (loadNumber > imgArr.length) {
@@ -101,31 +101,6 @@ renderArr.push(`<li class="projects-list-item">
 loadNumber += 3;
 return renderArr.join("")
 }
-
-
-
-// function renderProjectList (arr) {
-//     return arr.map(({url, urlX2, title}, i) => {
-//     return `<li class="projects-list-item">
-// <img
-//   class="projects-list-img"
-//   srcset="${url}, ${urlX2}"
-//   src="${url}"
-//   alt="${title}"
-//   width="320"
-// />
-// <p class="projects-list-text">React, JavaScript, Node JS, Git</p>
-// <h3 class="projects-list-title">${title}</h3>
-// <a class="projects-list-link" href="../index.html"
-//   >VISIT
-//   <svg class="projects-list-icon" width="24" height="24">
-//     <use href="./img/icons.svg#icon-arrow-up"></use>
-//   </svg>
-// </a>
-// </li>`
-// }).join("");
-// }
-
 
 
 

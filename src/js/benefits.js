d7c4d0310282d8a9d9160358c0cbe1cb2a174d40
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-06-17 15:07:35 +0300

    change benefits.js

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,18 +1,20 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
 const initializeBenefitsInfo = () => {
-  const benefitsList = document.querySelector('.benefits-list');
-  benefitsList.addEventListener('click', () => {
-    iziToast.info({
-      theme: 'dark',
-      message:
-        'Offer me a project right now and get all the benefits and even more!',
-      backgroundColor: '#00b068',
-      messageColor: '#FFF',
-      messageSize: '40px',
-      position: 'center',
-      maxWidth: 320,
+  const benefitsItems = document.querySelectorAll('.benefits-item');
+
+  benefitsItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      item.style.transform = 'scale(1.2)';
+      item.style.backgroundColor = '#F0F0F0';
+      item.style.transition = 'color 250ms cubic-bezier(0.4, 0, 0.2, 1)';
+      item.style.zIndex = '1';
+      item.style.paddingLeft = '5px';
+    });
+
+    item.addEventListener('mouseout', () => {
+      item.style.transform = 'scale(1)';
+      item.style.backgroundColor = 'transparent';
+      item.style.zIndex = '0';
+      item.style.paddingLeft = '0';
     });
   });
 };
